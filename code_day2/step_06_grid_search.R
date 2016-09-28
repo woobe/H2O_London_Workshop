f0ada17ef63ef5992bdda05d5a8b3c6e4cf21efc
Jo-fai Chow <jofai.chow@gmail.com>
2016-09-28 16:35:07 +0100

    v1

--- code_day2/step_06_grid_search.R
+++ code_day2/step_06_grid_search.R
@@ -74,3 +74,49 @@ best_gbm <- h2o.getModel(model_ids[[1]])
 # Evaluate
 h2o.performance(best_gbm, newdata = secom_test)
 
+
+
+
+# ------------------------------------------------------------------------------
+# Train H2O models with early stopping and RANDOM grid search
+# ------------------------------------------------------------------------------
+
+# Define parameters for grid search
+param_gbm <- list(
+  max_depth = c(4, 5, 6, 7, 8), sample_rate = c(0.6, 0.7, 0.8, 0.9, 1)
+)
+
+# Define criteria for random grid search
+search_criteria <- list(
+  strategy = "RandomDiscrete",
+  max_models = 5,
+  seed = 1234,
+  max_runtime_secs = 600
+)
+
+# GBM with early stopping, cross validation and RANDOM grid search
+ran_grid_gbm <- h2o.grid(
+
+  # Core parameters for model training
+  x = features, y = target,
+  training_frame = secom_train,
+  ntrees = 200,
+
+  # Parameters for grid search
+  grid_id = "random_gbm_grid",
+  hyper_params = param_gbm,
+  search_criteria = search_criteria, # <- added this for Random Grid Search
+  algorithm = "gbm",
+
+  # Parameters for early stopping & cross validation
+  nfolds = 5,
+  stopping_metric = "logloss",
+  score_tree_interval = 3,
+  stopping_rounds = 20
+
+)
+
+# Sort models by metric "logloss"
+ran_grid_sort <- h2o.getGrid("random_gbm_grid",
+                             sort_by = "logloss", decreasing = FALSE)
+print(ran_grid_sort)
