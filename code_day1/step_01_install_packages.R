beb54397314b41e5dd5f65da8cb8b2f769e6b52f
Jo-fai Chow <jofai.chow@gmail.com>
2016-11-20 12:23:13 +0000

    Updated code for H2O installation

--- code_day1/step_01_install_packages.R
+++ code_day1/step_01_install_packages.R
@@ -12,20 +12,25 @@ if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
 if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
 
 # Next, we download packages that H2O depends on.
-pkgs <- c("methods","statmod","stats","graphics","RCurl","jsonlite","tools","utils")
-for (pkg in pkgs) {
-  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
-}
+if (! ("methods" %in% rownames(installed.packages()))) { install.packages("methods") }
+if (! ("statmod" %in% rownames(installed.packages()))) { install.packages("statmod") }
+if (! ("stats" %in% rownames(installed.packages()))) { install.packages("stats") }
+if (! ("graphics" %in% rownames(installed.packages()))) { install.packages("graphics") }
+if (! ("RCurl" %in% rownames(installed.packages()))) { install.packages("RCurl") }
+if (! ("jsonlite" %in% rownames(installed.packages()))) { install.packages("jsonlite") }
+if (! ("tools" %in% rownames(installed.packages()))) { install.packages("tools") }
+if (! ("utils" %in% rownames(installed.packages()))) { install.packages("utils") }
 
 # Now we download, install and initialize the H2O package for R.
-install.packages("h2o",
-                 type="source",
-                 repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-turing/7/R")))
+install.packages("h2o", type="source", 
+                 repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-turing/10/R")))
 
 # Quick test
 suppressPackageStartupMessages(library(h2o))
 h2o.init(nthreads = -1)
 
+# Finally, let's run a demo to see H2O at work.
+demo(h2o.kmeans)
 
 # ------------------------------------------------------------------------------
 # Install "h2oEnsemble" for model stacking
